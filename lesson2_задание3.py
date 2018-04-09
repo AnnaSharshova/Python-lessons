82aa7f4e9b4f77bd636e4dca15c14e95c2fb57b5
Chepelova <38075014+Chepelova@users.noreply.github.com>
2018-04-09 23:21:41 +0700

    Add files via upload

---
+++ lesson2_задание3.py
@@ -0,0 +1,9 @@
+_dict = {"Maksimenko": "Eakaterina", "Moskalevich": "Karina", "Ivankova": "Anastasia", "Volkova": "Svetlana", 
+"Svechnikov": "Victor", "Sokolova": "Eugenia", "Dmitrieva": "Anna", "Okhotnikov": "Vitaly", "Chepelova": "Anna"}
+print (_dict ["Volkova"])
+
+_dict.update ({"Volodin": "Aleksey"})
+print (_dict)
+
+LastName = "Chepelova"
+print (_dict[LastName])
