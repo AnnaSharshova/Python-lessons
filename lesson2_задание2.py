b2238aa76973f00c6a704c32738902e47818b217
Chepelova <38075014+Chepelova@users.noreply.github.com>
2018-04-09 21:26:07 +0700

    Add files via upload

---
+++ lesson2_задание2.py
@@ -0,0 +1,7 @@
+_list = ["Anna", "Eugenya", "Karina", "Svetlana", "Anastasia", "Viktor", "Ekaterina", "Vitaly"]
+print (_list [1])
+print (_list [1:4])
+print (_list [4:])
+
+_list.append ("Anatoly")
+print (_list)
