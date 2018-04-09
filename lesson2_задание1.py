81517c0b6e64a6765c3a01b8fd6dea88ccf1eec4
Chepelova <38075014+Chepelova@users.noreply.github.com>
2018-04-09 21:13:54 +0700

    Add files via upload

--- lesson2_задание1.py
+++ lesson2_задание1.py
@@ -1,31 +1,23 @@
-_dict = {"1": "monday", "2": "tuesday",
-"3": "wednesday", "4": "thursday",
-"5": "friday", "6": "saturday",
-"7": "sunday"}
-print (_dict["1"])
+week_day = 3
+if week_day == 1:
+	print ("monday")
+elif week_day == 2:
+	print ("tuesday")
+elif week_day == 3:
+	print ("wednesday")
+elif week_day == 4:
+	print ("thursday")
+elif week_day == 5:
+	print ("friday")
+elif week_day == 6:
+	print ("saturday")
+else: 
+    print ("sunday")
 
 time = 13
-
-print (_dict["3"])
 if time < 12:
 	print ("a.m.")
 else:
 	print ("p.m.")
 
-# I didn't understand the task, but I tried
-
-week_day = 2
-if week_day < 2:
-	print ("monday")
-if week_day < 3:
-	print ("tuesday")
-if week_day < 4:
-	print ("wednesday")
-if week_day < 5:
-	print ("thursday")
-if week_day < 6:
-	print ("friday")
-if week_day < 7:
-	print ("saturday")
-else: 
-	print ("sunday")
+# I didn't understand the task, but I tried.
