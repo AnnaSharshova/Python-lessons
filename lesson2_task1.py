0fbbac6244730793e0f1b83c9e83d8245b117a48
Chepelova <38075014+Chepelova@users.noreply.github.com>
2018-04-10 09:37:45 +0700

    Update and rename lesson2_задание1.py to lesson2_task1.py

--- lesson2_task1.py
+++ lesson2_task1.py
@@ -8,24 +8,24 @@
 
 week_day = 3
 if week_day == 1:
-    print ("monday")
+    print("monday")
 elif week_day == 2:
-    print ("tuesday")
+    print("tuesday")
 elif week_day == 3:
-    print ("wednesday")
+    print("wednesday")
 elif week_day == 4:
-    print ("thursday")
+    print("thursday")
 elif week_day == 5:
-    print ("friday")
+    print("friday")
 elif week_day == 6:
-    print ("saturday")
+    print("saturday")
 else: 
-    print ("sunday")
+    print("sunday")
 
 time = 13
 if time < 12:
-	print ("a.m.")
+	print("a.m.")
 else:
-	print ("p.m.")
+	print("p.m.")
 
 # I didn't understand the task, but I tried.
